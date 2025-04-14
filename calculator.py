34baef39ebf956e4c52a707e9de561dfa489002a
Sunay Kanade <sunay_kanade@outlook.com>
2025-04-14 13:56:38 -0400

    added calculator functions
    added all calculator functions for partner 1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,34 @@
 """
 calculator.py
 - Defines functions used to create a simple calculator
-
 One function per operation, in order.
 """
+
+import math
+
 # First example
 def add(a, b): 
-    pass
+    return a + b
+
+def subtract(a, b): 
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0: 
+        raise ZeroDivisionError
+    return b/a
+
+def logarithm(a, b):
+    try:
+        return math.log(b, a)
+    except: 
+        raise ValueError
+
+def exponent(a, b):
+    return a ** b
+
 
 
