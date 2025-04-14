e69c7c5c59ce3e30f48661195e2e1e81c331654f
Sunay Kanade <sunay_kanade@outlook.com>
2025-04-14 19:04:32 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -24,7 +24,7 @@ def hypotenuse(a, b):
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
 def mul(a, b):
@@ -35,7 +35,7 @@ def div(a, b):
         raise ZeroDivisionError
     return b/a
 
-def log(a, b):
+def logarithm(a, b):
     try:
         return math.log(b, a)
     except: 
